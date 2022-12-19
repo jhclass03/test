61d39cd04a274b45c705fabe884687b98f5b70bd
jhclass03 <jhclass03@naver.com>
2022-12-20 02:22:34 +0900

    music_app_testing
    class testing..

---
+++ api/music.js
@@ -0,0 +1,62 @@
+let allMusic = [
+  {
+      name : "Alone",
+      artist : "Color Out",
+      img : "album1",
+      audio : "alone"
+  },
+  {
+      name : "Mahidevran Maze of sorrow",
+      artist : "Mahidevran Rock Band",
+      img : "malbum2",
+      audio : "Mahidevran-Maze_of_sorrow"
+  },
+  {
+      name : "No Rest Or Endless Rest",
+      artist : "Lisofv",
+      img : "album3",
+      audio : "No_Rest_Or_Endless_Rest"
+  },
+  {
+      name : "검정색하트 (Feat. Leellamarz, BE'O)",
+      artist : "TOIL",
+      img : "music-4",
+      audio : "music-4"
+  },
+  {
+      name : "우리서로사랑하지는말자",
+      artist : "기리보이",
+      img : "music-5",
+      audio : "music-5"
+  },
+  {
+      name : "BROKEN FERRARI",
+      artist : "호미들",
+      img : "music-6",
+      audio : "music-6"
+  },
+  {
+      name : "만남은 쉽고 이별은 어려워 (Feat. Leellamarz) (Prod. TOIL)",
+      artist : "베이식 (Basick)",
+      img : "music-7",
+      audio : "music-7"
+  },
+  {
+      name : "거짓말 (Feat. BIG Naughty)",
+      artist : "김승민",
+      img : "music-8",
+      audio : "music-8"
+  },
+  {
+      name : "이혼서류",
+      artist : "기리보이",
+      img : "music-9",
+      audio : "music-9"
+  },
+  {
+      name : "봄 (Feat. ASH ISLAND, Skinny Brown)",
+      artist : "TOIL",
+      img : "music-10",
+      audio : "music-10"
+  },
+]
